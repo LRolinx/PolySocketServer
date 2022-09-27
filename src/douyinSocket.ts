79d15c058583d16b1eb2cf59b5a5bb4f693a7796
LRolinx <yollx@qq.com>
2022-09-27 10:29:55 +0800

    优化抖音弹幕获取的写法逻辑

--- src/douyinSocket.ts
+++ src/douyinSocket.ts
@@ -98,81 +98,94 @@ export default class DouYinSocket {
         dataview.setUint16(2, 0x6862);
         ws.send(dataview.buffer); //发送
     }
-}
 
-//抖音10秒一个心跳
-//心跳内容00000000:3a02 6862
-
-
-
-
-
-
-
-
-function handleWebcast(
-    roomId: string,
-    cb: (name: string, content: string, html: string) => void
-) {
-    puppeteer
-        .launch({
-            devtools: false,
-            slowMo: 400,
-            defaultViewport: { width: 1280, height: 800 },
-        })
-        .then(async (browser) => {
-            const page = await browser.newPage();
-            await page.goto(`https://live.douyin.com/${roomId}`);
-            // 放一个函数到window上,并且这个函数会在node中执行
-            await page.exposeFunction(
-                "addMessage",
-                (name: string, content: string, html: string) => {
-                    if (content && name && html) {
-                        cb(name, content, html);
+    handleWebcast(cb: (name: string, content: string, html: string) => void) {
+        puppeteer
+            .launch({
+                devtools: false,
+                slowMo: 400,
+                defaultViewport: { width: 1280, height: 800 },
+            })
+            .then(async (browser) => {
+                const page = await browser.newPage();
+                await page.goto(`https://live.douyin.com/${this.roomid}`);
+                // 放一个函数到window上,并且这个函数会在node中执行
+                await page.exposeFunction(
+                    "addMessage",
+                    (name: string, content: string, html: string) => {
+                        
+                        if (content && name && html) {
+                            cb(name, content, html);
+                        }
                     }
-                }
-            );
+                );
 
-            await page.evaluate(() => {
-                function getElementTextWithImg(parent: Node | null) {
-                    if (!parent || !parent.childNodes) {
-                        return "";
-                    }
-                    let content = "";
-                    for (const node of parent.childNodes) {
-                        if (node.nodeType === 1) {
-                            for (const child of node.childNodes) {
-                                if (child.nodeType === 3) {
-                                    //文本
-                                    content += (child as Text).data;
-                                } else if (child.nodeType === 1) {
-                                    // 元素
-                                    const el = child as HTMLElement;
-                                    if (el.tagName === "IMG") {
-                                        content += (el as HTMLImageElement).alt || "";
+                await page.evaluate(() => {
+                    function getElementTextWithImg(parent: Node | null) {
+                        if (!parent || !parent.childNodes) {
+                            return "";
+                        }
+                        let content = "";
+                        for (const node of parent.childNodes) {
+                            if (node.nodeType === 1) {
+                                for (const child of node.childNodes) {
+                                    if (child.nodeType === 3) {
+                                        //文本
+                                        content += (child as Text).data;
+                                    } else if (child.nodeType === 1) {
+                                        // 元素
+                                        const el = child as HTMLElement;
+                                        if (el.tagName === "IMG") {
+                                            content += (el as HTMLImageElement).alt || "";
+                                        }
                                     }
                                 }
                             }
                         }
+                        return content;
                     }
-                    return content;
-                }
-                const items = document.querySelector(
-                    ".webcast-chatroom___items > div"
-                ) as HTMLDivElement;
-                const appendChild = items.appendChild;
-                items.appendChild = function (n: Node) {
-                    const node = n as Element;
-                    const html = node.innerHTML;
-                    const div = node.childNodes[0] as HTMLDivElement;
 
-                    const name = div.childNodes[1]?.textContent || "";
-                    const content = getElementTextWithImg(div.childNodes[2]);
-                    (window as any)["addMessage"](name, content, html);
+                    // //截取用户发的消息
+                    const items = document.querySelector(
+                        ".webcast-chatroom___items > div"
+                    ) as HTMLDivElement;
+                    const appendChild = items.appendChild;
+                    items.appendChild = function (n: Node) {
+                        const node = n as Element;
+                        const html = node.innerHTML;
+                        const div = node.childNodes[0] as HTMLDivElement;
 
-                    return appendChild.call(this, node) as any;
-                };
+                        const name = div.childNodes[1]?.textContent?.replace(":", "") || "";
+                        const content = getElementTextWithImg(div.childNodes[2]);
+
+                        (window as any)["addMessage"](name, content, html);
+
+                        return appendChild.call(this, node) as any;
+                    };
+
+                    // //截取有人进来了
+                    // const enter = document.querySelector(
+                    //     ".webcast-chatroom___bottom-message"
+                    // ) as HTMLDivElement;
+                    // const enterappendChild = enter.appendChild;
+                    // enter.appendChild = function (n: Node) {
+                    //     const node = n as Element;
+                    //     const html = node.innerHTML;
+                    //     const div = node.childNodes[0] as HTMLDivElement;
+
+                    //     const name = div.childNodes[1]?.textContent || "";
+                    //     const content = getElementTextWithImg(div.childNodes[2]);
+                    //     (window as any)["addMessage"](name, content, html);
+
+                    //     return enterappendChild.call(this, node) as any;
+                    // };
+
+
+                });
+                console.log((new Date()).toLocaleTimeString(), `DouYin已进入${this.roomid}号房间`);
             });
-            console.log(`抖音弹幕抓取初始化完成!`);
-        });
+    }
 }
+
+//抖音10秒一个心跳
+//心跳内容00000000:3a02 6862
