7a638f659b8fa54034f71bc605b5c1d3f4f78f5a
LRolinx <yollx@qq.com>
2022-09-27 10:30:24 +0800

    添加抖音的转发方法，并改正B站转发的内容

--- src/datapipe.ts
+++ src/datapipe.ts
@@ -85,10 +85,10 @@ class datapipe {
     }
 
     /**
-     * 弹幕
+     * BiliBili弹幕
      * @param data 
      */
-    danmaku = async (data: any) => {
+    bilibilidanmaku = async (data: any) => {
         if (this.clients.length >= 0 || this.saveDataPath != "") {
             //没获取到保存资源路径和没有任何需要转发的客户端则不转发弹幕
             let miniMsg = {}
@@ -163,7 +163,143 @@ class datapipe {
                         price: data.data.price,
                     }
                     console.log((new Date()).toLocaleTimeString(), "送礼", data)
-                    
+
+                    this.pub(miniMsg);
+                    break;
+                case 'COMBO_SEND':
+                    //组合送礼
+                    // console.log("组合送礼", data)
+                    break;
+                case 'WATCHED_CHANGE':
+                    //观看变化
+                    // console.log('观看变化', data);//数据格式从打印中就可以分析出来啦
+                    break;
+                case 'ROOM_REAL_TIME_MESSAGE_UPDATE':
+                    //在线房间列表
+                    break;
+                case 'STOP_LIVE_ROOM_LIST':
+                    //离线房间列表
+                    break;
+                case 'LIVE_INTERACTIVE_GAME':
+                    //现场互动游戏
+                    break;
+                case 'NOTICE_MSG':
+                    //通知消息
+                    break;
+                case 'SUPER_CHAT_MESSAGE':
+                    //醒目留言
+                    break;
+                case 'GUARD_BUY':
+                    //续费舰长
+                    miniMsg = {
+                        cmd: "BiliBili_SEND_GIFT",
+                        uid: data.data.uid,
+                        name: data.data.username,
+                        giftName: data.data.gift_name,
+                        giftId: data.data.gift_id,
+                        giftType: data.data.giftType,
+                        num: data.data.num,
+                        price: data.data.price,
+                    }
+                    this.pub(miniMsg);
+
+                    break;
+                case 'USER_TOAST_MSG':
+                    //续费舰长通知
+                    break;
+                case 'LIVE':
+                    //直播开始啦
+                    break;
+                case 'ROOM_CHANGE':
+                    //更改直播信息
+                    break;
+                default:
+                    console.log((new Date()).toLocaleTimeString(), '---未确认格式---', data);
+            }
+            this.onMessage(data)
+        }
+    }
+
+    /**
+ * Douyin弹幕
+ * @param data 
+ */
+    douyindanmaku = async (data: any) => {
+        if (this.clients.length >= 0 || this.saveDataPath != "") {
+            //没获取到保存资源路径和没有任何需要转发的客户端则不转发弹幕
+            let miniMsg = {}
+            switch (data.cmd) {
+                case 'DANMU_MSG':
+                    //普通弹幕
+                    console.log((new Date()).toLocaleTimeString(), data.info[2][1], "->", data.info[1])
+                    miniMsg = {
+                        cmd: "BiliBili_DANMU_MSG",
+                        text: data.info[1],
+                        uid: data.info[2][0],
+                        name: data.info[2][1],
+                    }
+
+                    this.pub(miniMsg);
+
+                    break;
+                case 'SUPER_CHAT_MESSAGE_JPN':
+                    //超级弹幕
+                    // console.log((new Date()).toLocaleTimeString(),"超级弹幕", data)
+
+                    miniMsg = {
+                        cmd: "BiliBili_DANMU_MSG",
+                        text: data.data.message,
+                        uid: data.data.uid,
+                    }
+
+                    console.log((new Date()).toLocaleTimeString(), "超级弹幕->", miniMsg)
+
+                    this.pub(miniMsg);
+                case 'ENTRY_EFFECT':
+                    //进入直播特效
+                    break;
+                case 'INTERACT_WORD':
+                    //进入直播
+                    // console.log(data.data)
+                    // console.log("进入直播: " + data.data.uname);
+
+                    miniMsg = {
+                        cmd: "BiliBili_DANMU_MSG",
+                        text: "我来了~",
+                        uid: data.data.uid,
+                        name: data.data.uname,
+                    }
+
+                    console.log((new Date()).toLocaleTimeString(), miniMsg)
+
+                    this.pub(miniMsg);
+                    break;
+                case 'ONLINE_RANK_COUNT':
+                    //直播排名
+                    break;
+                case 'ONLINE_RANK_V2':
+                    ////在线排名V2
+                    break;
+                case 'HOT_RANK_CHANGED':
+                    //热门排行
+                    break;
+                case 'HOT_RANK_CHANGED_V2':
+                    //热门排名V2
+                    break;
+                case 'SEND_GIFT':
+                    //送礼物
+                    miniMsg = {
+                        cmd: "BiliBili_SEND_GIFT",
+                        uid: data.data.uid,
+                        name: data.data.uname,
+                        giftName: data.data.giftName,
+                        giftId: data.data.giftId,
+                        giftType: data.data.giftType,
+                        num: data.data.num,
+                        price: data.data.price,
+                    }
+                    console.log((new Date()).toLocaleTimeString(), "送礼", data)
+
                     this.pub(miniMsg);
                     break;
                 case 'COMBO_SEND':
